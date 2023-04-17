6751d447bf6d59bd9d6fdb6f092cdeaa9b9bfc26
Mika Munterud <mika.munterud@outlook.com>
2023-04-17 23:26:01 +0200

    Creating a global state to handle if the user is logged in or not and adding it to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 //Dependencies
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //Style
-import './App.css';
+import "./sass/App.scss";
 //Components
-import Dummy from './pages/Dummy'
+import Dummy from "./pages/Dummy";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { AuthenticationProvider } from "./contexts/Authentication";
 
 function App() {
   return (
-    <BrowserRouter>
-      <header className='Appheader'>
-        <h1>TravelFlow</h1>
-
-      </header>
-      <Routes>
-        <Route path="/" element={<Dummy />} />
-        <Route path="/login" element={<Dummy />} />
-        <Route path="/register" element={<Dummy />} />
-        <Route path="/user/:username" element={<Dummy />} />
-        <Route path="/feed" element={<Dummy />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthenticationProvider>
+      <BrowserRouter>
+        <header className="Appheader">
+          <h1>TravelFlow</h1>
+        </header>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/user/:username" element={<Dummy />} />
+          <Route path="/feed" element={<Dummy />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthenticationProvider>
   );
 }
 
