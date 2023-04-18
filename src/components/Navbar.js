17c6de33575b6a8021d17a4269653a902595c3b5
TereseBo <terese.boderus@gmail.com>
2023-04-18 15:02:11 +0200

    Removed placeholder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@
 import './Navbar.css'
 export default function Navbar({ children }) {
     return (<div className="Navbar">
-        <p>Children incoming</p>
         {children.map((child, index) => (
             <div key={`Nav-child-${index}-container`}className='Child-container'>
                 {child}
