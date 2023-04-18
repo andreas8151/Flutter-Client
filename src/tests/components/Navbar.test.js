f451dc6f6c076bdc7715a7f46e864d592d251e41
TereseBo <terese.boderus@gmail.com>
2023-04-18 15:09:13 +0200

    Spelling-fix

--- src/tests/components/Navbar.test.js
+++ src/tests/components/Navbar.test.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import App from '../../App'
 import Navbar from '../../components/Navbar';
 
-test('renders navbar with wisible link children', () => {
+test('renders navbar with visible link children', () => {
   render(<App><Navbar>
     <Link to="/login">Login</Link>
     <Link to="/register">Register</Link>
