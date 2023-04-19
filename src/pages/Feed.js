d7c7fd97bd471c78df58ca0dec5caf2cca8489d9
Mika Munterud <mika.munterud@outlook.com>
2023-04-20 01:41:05 +0200

    Adding initial Feed file structure

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,1 +1,22 @@
-export default function Feed() {}
+import { useContext, useEffect, useState } from "react";
+import { AuthenticationContext } from "../contexts/AuthenticationContext";
+
+export default function Feed() {
+  const [feeds, setFeeds] = useState([]);
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthenticationContext);
+
+  useEffect(function () {
+    async function getFeeds() {}
+    getFeeds();
+  });
+
+  if (!isLoggedIn) {
+    return (
+      <section className="mainSection">
+        <h2>Please login to access your feeds</h2>
+      </section>
+    );
+  }
+
+  return <section className="mainSection"></section>;
+}
