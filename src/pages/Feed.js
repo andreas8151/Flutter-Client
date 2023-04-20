57a4251d499d60e744045255c4542182ba00a084
Mika Munterud <mika.munterud@outlook.com>
2023-04-20 13:27:18 +0200

    Adding initial Feed route

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,22 +1,54 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthenticationContext } from "../contexts/AuthenticationContext";
+import PostBox from "../components/PostBox";
 
 export default function Feed() {
   const [feeds, setFeeds] = useState([]);
   const { isLoggedIn, setIsLoggedIn } = useContext(AuthenticationContext);
 
   useEffect(function () {
-    async function getFeeds() {}
     getFeeds();
   });
+
+  async function getFeeds() {
+    try {
+      const response = await fetch("http://localhost:5050/posts/following", {
+        method: "GET",
+        credentials: "include",
+      });
+
+      if (response.status === 401) {
+        setIsLoggedIn(false);
+        return;
+      }
+
+      if (response.status === 404) {
+        const responseMessage = await response.json();
+        return console.log(responseMessage.error);
+      }
+
+      if (response.status === 200) {
+        const responseMessage = await response.json();
+        return setFeeds(responseMessage.posts);
+      }
+    } catch (FetchError) {
+      return console.log(FetchError);
+    }
+  }
 
   if (!isLoggedIn) {
     return (
       <section className="mainSection">
-        <h2>Please login to access your feeds</h2>
+        <h2 className="notLoggedIn">Please login to access your feeds</h2>
       </section>
     );
   }
 
-  return <section className="mainSection"></section>;
+  return (
+    <section className="mainSection">
+      {feeds.map(function (post, index) {
+        return <PostBox key={index} post={post} getFeeds={getFeeds} />;
+      })}
+    </section>
+  );
 }
