4b87714b72c293211f5b2579e0e90b10b149e155
Mika Munterud <mika.munterud@outlook.com>
2023-04-20 23:29:06 +0200

    Adding global user context to application

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 //Global context
 import { AuthenticationProvider } from "./contexts/AuthenticationContext";
+import { UsersProvider } from "./contexts/UsersContext";
 //Style
 import "./sass/App.scss";
 //Components
@@ -15,25 +16,27 @@ import Feed from "./pages/Feed";
 function App() {
   return (
     <AuthenticationProvider>
-      <BrowserRouter>
-        <header className="Appheader">
-          <h1>TravelFlow</h1>
-          <Navbar>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-            <Link to="/user/:username">Profile</Link>
-            <Link to="/feed"></Link>
-          </Navbar>
-        </header>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/users/:username" element={<Dummy />} />
-          <Route path="/feed" element={<Feed />} />
-        </Routes>
-      </BrowserRouter>
+      <UsersProvider>
+        <BrowserRouter>
+          <header className="Appheader">
+            <h1>TravelFlow</h1>
+            <Navbar>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+              <Link to="/user/:username">Profile</Link>
+              <Link to="/feed"></Link>
+            </Navbar>
+          </header>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/users/:username" element={<Dummy />} />
+            <Route path="/feed" element={<Feed />} />
+          </Routes>
+        </BrowserRouter>
+      </UsersProvider>
     </AuthenticationProvider>
   );
 }
