90a3acc3a5a1744e7a3865b9d50ff5c266367eaa
Mika Munterud <mika.munterud@outlook.com>
2023-04-20 23:31:46 +0200

    Adding global user context to store all registered users

--- src/contexts/UsersContext.js
+++ src/contexts/UsersContext.js
@@ -5,12 +5,42 @@ export function UsersProvider({ children }) {
   const [users, setUsers] = useState();
   const [loading, setLoading] = useState(true);
 
-  useEffect(function () {}, []);
+  useEffect(
+    function () {
+      async function getUsers() {
+        try {
+          const response = await fetch("http://localhost:5050/users/allUsers", {
+            method: "GET",
+            headers: {
+              "content-type": "application/json",
+            },
+            credentials: "include",
+          });
+
+          if (response.status === 404) {
+            setUsers([]);
+          }
+
+          if (response.status === 200) {
+            const serverObject = await response.json();
+            setUsers(serverObject.users);
+          }
+        } catch (error) {
+          setUsers(false);
+        }
+        setLoading(false);
+      }
+      getUsers();
+    },
+    [users]
+  );
 
   if (loading) {
-    <section className="mainSection">
-      <h3>Loading...</h3>
-    </section>;
+    return (
+      <section className="mainSection">
+        <h3>Loading...</h3>
+      </section>
+    );
   }
 
   return (
