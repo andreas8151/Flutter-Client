b28ab5c68d929f5bebb682e154311452ac5e6f79
Mika Munterud <mika.munterud@outlook.com>
2023-04-22 00:03:28 +0200

    Moving navbar to separate folder and adjusting related files directory

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+//Style
+import "../Navbar.css";
+export default function Navbar({ children }) {
+  return (
+    <div className="Navbar">
+      {children.map((child, index) => (
+        <div key={`Nav-child-${index}-container`} className="Child-container">
+          {child}
+        </div>
+      ))}
+    </div>
+  );
+}
