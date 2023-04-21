c56e3cbf0ccd8d097dc958fc16e839c8bbae0c7e
Johan Bäcklin <112544750+johanbacklin@users.noreply.github.com>
2023-04-21 17:20:47 +0200

    Added a new map function &new fetch posts/username

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { AuthenticationContext } from "../contexts/AuthenticationContext";
+import PostBox from "../components/PostBox";
 
 export default function Profile() {
   const [postText, setPostText] = useState("");
+  const [feeds, setFeeds] = useState([]);
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthenticationContext);
 
-  /*  function handlePostText(event) {
-    event.preventDefault();
-    setPostText(event.target.value);
-  } */
+  const { username } = JSON.parse(localStorage.getItem("loggedInUser"));
 
   async function postFetch() {
     try {
@@ -25,6 +26,35 @@ export default function Profile() {
     }
   }
 
+  useEffect(function () {
+    getFeeds();
+  }, []);
+
+  async function getFeeds() {
+    try {
+      const response = await fetch(`http://localhost:5050/posts/${username}`, {
+        method: "GET",
+        credentials: "include",
+      });
+
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+
+      setFeeds(data);
+    } catch (FetchError) {
+      return console.log(FetchError);
+    }
+  }
+
+  if (!isLoggedIn) {
+    return (
+      <section className="mainSection">
+        <h2 className="notLoggedIn">Please login to access your feeds</h2>
+      </section>
+    );
+  }
+
   return (
     <>
       <form onSubmit={postFetch}>
@@ -34,6 +64,17 @@ export default function Profile() {
         ></textarea>
         <button type="submit">Submit</button>
       </form>
+
+      <section className="mainSection">
+        {feeds.map(function (post, index) {
+          console.log(post.username, username);
+          if (post.username === username) {
+            return <PostBox key={index} post={post} getFeeds={getFeeds} />;
+          } else {
+            return <PostBox key={index} post={post} />;
+          }
+        })}
+      </section>
     </>
   );
 }
