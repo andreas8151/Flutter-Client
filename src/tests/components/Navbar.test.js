0851bdae312b8c9bc0406d999e1596fc199aa6a4
Mika Munterud <mika.munterud@outlook.com>
2023-04-21 10:01:39 +0200

    Fixing navbar testing outcome text

--- src/tests/components/Navbar.test.js
+++ src/tests/components/Navbar.test.js
@@ -1,13 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import { Link } from 'react-router-dom';
-import App from '../../App'
-import Navbar from '../../components/Navbar';
+import { render, screen } from "@testing-library/react";
+import { Link } from "react-router-dom";
+import App from "../../App";
+import Navbar from "../../components/Navbar";
 
-test('renders navbar with visible link children', () => {
-  render(<App><Navbar>
-    <Link to="/login">Login</Link>
-    <Link to="/register">Register</Link>
-  </Navbar></App>);
-  const loginLink = screen.getByText(/Login/i);
+test("renders navbar with visible link children", () => {
+  render(
+    <App>
+      <Navbar>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </Navbar>
+    </App>
+  );
+  const loginLink = screen.getByText(/Loading/i);
   expect(loginLink).toBeInTheDocument();
 });
