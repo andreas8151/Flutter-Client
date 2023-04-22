20d7b624204e1430fccad5d1c9bd2541a3e12b81
Mika Munterud <mika.munterud@outlook.com>
2023-04-22 17:18:33 +0200

    Updating paths & global contexts imports to app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 //Dependencies
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+//Global context
+import { AuthenticationProvider } from "./contexts/AuthenticationContext";
+import { UsersProvider } from "./contexts/UsersContext";
 //Style
 import "./sass/App.scss";
 //Components
 import Dummy from "./pages/Dummy";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import { AuthenticationProvider } from "./contexts/Authentication";
 import Navbar from "./components/navbar/Navbar";
+import Users from "./pages/Users";
+import Feed from "./pages/Feed";
 
 function App() {
   return (
     <AuthenticationProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/user/" element={<Dummy />} />
-          <Route path="/user/:username" element={<Dummy />} />
-          <Route path="/feed" element={<Dummy />} />
-        </Routes>
-      </BrowserRouter>
+      <UsersProvider>
+        <BrowserRouter>
+          <Navbar />
+
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/users/:username" element={<Dummy />} />
+            <Route path="/feed" element={<Feed />} />
+          </Routes>
+        </BrowserRouter>
+      </UsersProvider>
     </AuthenticationProvider>
   );
 }
