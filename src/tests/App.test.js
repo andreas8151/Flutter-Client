405257bdf1764f6951a000079e9df05b8b2731f7
Mika Munterud <mika.munterud@outlook.com>
2023-04-22 17:20:14 +0200

    Updating test element

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render, screen } from "@testing-library/react";
+import App from "../App";
 
-test('renders learn react link', () => {
+test("rendered App has expected heading", () => {
   render(<App />);
-  const linkElement = screen.getByText(/TravelFlow/i);
+  const linkElement = screen.getByText(/Loading/i);
   expect(linkElement).toBeInTheDocument();
 });
