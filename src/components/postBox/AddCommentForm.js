665e88356ac5ddfbffe3c784a029fd182500418f
Mika Munterud <mika.munterud@outlook.com>
2023-04-24 13:38:57 +0200

    Adding function to add comment to a post in AddCommentForm

--- src/components/postBox/AddCommentForm.js
+++ src/components/postBox/AddCommentForm.js
@@ -1,9 +1,65 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import Swal from "sweetalert2";
+import { AuthenticationContext } from "../../contexts/AuthenticationContext";
+import { getFeeds } from "../../functions/getFeeds";
 
-export default function AddCommentForm({ postID, index }) {
+export default function AddCommentForm({ postID, setFeeds }) {
+  const { setIsLoggedIn } = useContext(AuthenticationContext);
   const [words, setWords] = useState(0);
+  const [text, setText] = useState("");
 
-  async function addComment() {}
+  async function addComment(event) {
+    event.preventDefault();
+
+    const commentText = text.trim();
+    try {
+      const response = await fetch("http://localhost:5050/posts/comment", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({ postID, commentText }),
+      });
+
+      if (response.status === 401) {
+        await Swal.fire({
+          icon: "error",
+          text: "Please login to be able to comment a users post!",
+        });
+        localStorage.removeItem("loggedInUser");
+        setIsLoggedIn(false);
+        return;
+      }
+
+      if (response.status === 200) {
+        const { loggedIn, feedList } = await getFeeds();
+        await setIsLoggedIn(loggedIn);
+        setText("");
+        setWords(0);
+        if (loggedIn) {
+          setFeeds(feedList);
+        }
+        return;
+      }
+      const responseMessage = await response.text();
+      const responseJSON = await response.json();
+      if (responseMessage) {
+        await Swal.fire({ icon: "error", text: responseMessage });
+      }
+      if (responseJSON) {
+        await Swal.fire({ icon: "error", text: responseJSON });
+      }
+      return;
+    } catch (FetchError) {
+      await Swal.fire({
+        icon: "error",
+        text: "Something went wrong, failed to connect to server!",
+      });
+      setIsLoggedIn("serverError");
+      return;
+    }
+  }
 
   return (
     <form className="addCommentForm" onSubmit={addComment}>
@@ -22,9 +78,11 @@ export default function AddCommentForm({ postID, index }) {
         id={`comment${postID}`}
         maxLength={200}
         rows={3}
+        value={text}
         placeholder="Add comment..."
         onChange={function (event) {
           setWords(event.target.textLength);
+          setText(event.target.value);
         }}
       />
       <button className="addCommentForm_button">Add comment</button>
