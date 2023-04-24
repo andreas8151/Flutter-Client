addd8904ff3d3eead098268f490d4e652b9dde77
Mika Munterud <mika.munterud@outlook.com>
2023-04-24 10:57:41 +0200

    Fixing navbar test to use changed filename Header

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,15 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import { Link } from "react-router-dom";
 import App from "../../App";
-import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/navbar/Header";
 
 test("renders navbar with visible link children", () => {
   render(
     <App>
-      <Navbar>
+      <Header>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
-      </Navbar>
+      </Header>
     </App>
   );
   const loginLink = screen.getByText(/Loading/i);
