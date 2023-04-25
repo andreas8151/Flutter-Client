e91a2dccf1ade46bc0cce9d5337adb9d2b1b0da4
Johan Bäcklin <112544750+johanbacklin@users.noreply.github.com>
2023-04-25 19:41:55 +0200

    Cleaned up code

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -49,15 +49,7 @@ export default function Feed() {
   return (
     <section className="mainSection" id="feed">
       {feeds.map(function (post, index) {
-        return (
-          <PostBox
-            key={index}
-            post={post}
-            setFeeds={setFeeds}
-            showButtons={false}
-            index={index}
-          />
-        );
+        return <PostBox key={index} post={post} setFeeds={setFeeds} />;
       })}
     </section>
   );
